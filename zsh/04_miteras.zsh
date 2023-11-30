711a469369e8cbe0523daf3ce0cc0a87808165cc
d8y <16472010+d8y@users.noreply.github.com>
2023-12-01 08:57:40 +0900

    update 04_miteras.zsh

--- zsh/04_miteras.zsh
+++ zsh/04_miteras.zsh
@@ -1,8 +1,18 @@
+function hello() {
+  open -a MiTERASWVAgent
+
+  curl --request POST \
+           --url https://api.chatwork.com/v2/rooms/329656715/messages \
+           --header "X-ChatWorkToken:  ${CHAT_WORK_TOKEN}" \
+           --header 'accept: application/json' \
+           --header 'content-type: application/x-www-form-urlencoded' \
+           --data self_unread=0 \
+           --data 'body=おはようございます。業務開始します'
+}
+
+
 function killMiteras() {
-    pgrep MiTERASWVAgent | read pid;
-    if [[ -n $pid ]]; then
-        kill $pid
-    fi
+    pkill MiTERASWVAgent
 
     curl --request POST \
          --url https://api.chatwork.com/v2/rooms/329656715/messages \
